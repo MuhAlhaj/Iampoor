eac7999092a942393a758e38eca264e2dbde306e
Muhammad Alhaj <MuhALhaj@gmail.com>
2018-04-13 15:55:45 +0300

    Commit file v1

---
+++ app/src/main/java/com/alhaj/i_am_poor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.alhaj.i_am_poor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
